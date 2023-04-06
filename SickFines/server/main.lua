8075b36525e415a71e04ce22d557b5322e1148e2
SickJuggalo666 <82001106+sickjuggalo666@users.noreply.github.com>
2023-04-06 08:06:05 -0600

    getting close

--- SickFines/server/main.lua
+++ SickFines/server/main.lua
@@ -4,7 +4,7 @@ local Bank = exports.pefcl
 
 local Inventory = exports.ox_inventory
 
-Citizen.CreateThread(function()
+--[[Citizen.CreateThread(function()
     for k, v in pairs(Config.Jobs) do
         local tickets = {
             id = v.id,
@@ -16,13 +16,35 @@ Citizen.CreateThread(function()
 
         Inventory:RegisterStash(tickets.id, tickets.label, tickets.slots, tickets.maxWeight, nil, tickets.groups)
     end
-end)
+end)]]
+
+local Discord_url = '' -- server side to protect you webhooks
+
+local function LetEmKnow(color, name, message, footer)
+    local embed = {
+        {
+            ["color"] = 3085967,
+            ["title"] = "**".. name .."**",
+            ["description"] = message,
+            ["footer"] = {
+                ["text"] = footer,
+            },
+            ["author"] = {
+            ["name"] = 'Made by | SickJuggalo666',
+            ['icon_url'] = 'https://i.imgur.com/arJnggZ.png'
+            }
+        }
+    }
+      
+    PerformHttpRequest(Discord_url, function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
+end
 
 RegisterNetEvent('SickFines:CheckInvoices')
 AddEventHandler('SickFines:CheckInvoices', function(id, amount, reason, PlayerName, OfficerName, date, Job)
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
     local target = ESX.GetPlayerFromId(id)
+    if xPlayer ~= nil then return end
     Bank:createInvoice(source, {
         to = PlayerName,
         toIdentifier = target.identifier,
@@ -39,12 +61,11 @@ AddEventHandler('SickFines:CheckInvoices', function(id, amount, reason, PlayerNa
         OfficerName = OfficerName,
         date = date
     }
-
-    Inventory:AddItem('police_tickets', 'ticket', 1, info )
-    
-    TriggerEvent('SickFines:LetEmKnow', amount, reason, PlayerName, OfficerName, date)
-
+    if exports.ox_inventory:CanCarryItem(inv, item, count, metadata) then
+        Inventory:AddItem(source, 'ticket', 1, info )
+        Inventory:AddItem(id, 'ticket', 1, info )
+    end
+    local name = "Sick Ticket System"
+    local message = (('%s Issued a Ticket to %s, \nReaon: %s, \nAmount $:'..amount):format(OfficerName,PlayerName,reason,amount))
+    LetEmKnow(name,message)
 end)
-
-RegisterNetEvent('SickFines:LetEmKnow')
-AddEventHandler('SickFines:LetEmKnow', )
